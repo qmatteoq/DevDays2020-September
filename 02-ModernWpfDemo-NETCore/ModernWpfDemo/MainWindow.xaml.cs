36193cbcd5d5abd7882e1f48e0949ceff05f2cca
Matteo Pagani <matteo.pagani@microsoft.com>
2020-09-09 14:44:35 +0200

    Added starting point

--- 02-ModernWpfDemo-NETCore/ModernWpfDemo/MainWindow.xaml.cs
+++ 02-ModernWpfDemo-NETCore/ModernWpfDemo/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ namespace ModernWpfDemo
             LongBlock.Text = longitude;
             LatBlock.Text = latitude;
 
-            await TheMap.TrySetViewAsync(pos.Coordinate.Point, 15.0);
+            //await TheMap.TrySetViewAsync(pos.Coordinate.Point, 15.0);
         }
     }
 }
