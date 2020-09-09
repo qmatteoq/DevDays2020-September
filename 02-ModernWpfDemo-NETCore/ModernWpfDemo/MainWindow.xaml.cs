76b7df9f1b99cff35103c0f7544a4fb77c64e9ed
Matteo Pagani <matteo.pagani@microsoft.com>
2020-09-09 16:03:54 +0200

    Updated main demo

--- 02-ModernWpfDemo-NETCore/ModernWpfDemo/MainWindow.xaml.cs
+++ 02-ModernWpfDemo-NETCore/ModernWpfDemo/MainWindow.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Wpf.UI.XamlHost;
+using System;
 using Windows.Devices.Geolocation;
+using Windows.UI.Xaml.Controls;
 
 namespace ModernWpfDemo
 {
     public partial class MainWindow
     {
+        private ProgressRing progressRing;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -12,6 +16,8 @@ namespace ModernWpfDemo
 
         private async void Button_Click(object sender, System.Windows.RoutedEventArgs e)
         {
+            progressRing.IsActive = true;
+
             Geolocator geolocator = new Geolocator() { DesiredAccuracyInMeters = 5 };
             Geoposition pos = await geolocator.GetGeopositionAsync();
             string longitude = pos.Coordinate.Point.Position.Longitude.ToString();
@@ -19,7 +25,16 @@ namespace ModernWpfDemo
             LongBlock.Text = longitude;
             LatBlock.Text = latitude;
 
-            await TheMap.TrySetViewAsync(pos.Coordinate.Point, 15.0);
+            progressRing.IsActive = false;
+        }
+
+        private void WindowsXamlHost_ChildChanged(object sender, EventArgs e)
+        {
+            if (sender is WindowsXamlHost xamlHost && xamlHost.Child is ProgressRing ring)
+            {
+                progressRing = ring;
+                progressRing.Width = 150;
+            }
         }
     }
 }
